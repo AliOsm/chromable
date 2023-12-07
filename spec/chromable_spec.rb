578119278e15c46bca6c0bf5964c833b2d96433b
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2023-12-08 00:49:44 +0300

    Add GitHub Actions for rspec

--- spec/chromable_spec.rb
+++ spec/chromable_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Chromable do
   it 'has a version number' do
     expect(Chromable::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
